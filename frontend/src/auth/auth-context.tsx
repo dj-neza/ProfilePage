ccc7cb6d8d7df036a0e98156f13c2a5131346595
dj-neza <neza.djukic@gmail.com>
2023-11-01 23:36:54 +0100

    Add firebise init to the auth context

--- frontend/src/auth/auth-context.tsx
+++ frontend/src/auth/auth-context.tsx
@@ -1,4 +1,7 @@
-import { ReactNode, useContext, useState } from "react";
+import { ReactNode, createContext, useContext, useState } from "react";
+import { firebaseConfig } from "../vendor/firebase-config";
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
 
 type User = {
   name: string;
@@ -7,9 +10,18 @@ type User = {
 };
 
 type AuthContextType = {
-  user: User;
-  setUser: (authBody: User) => void;
+  user: User | null;
+  setUser: (user: User) => void;
+  firebaseAuth: firebase.auth.Auth;
+  firebaseUiConfig: {
+    signInFlow: string;
+    signInSuccessUrl: string;
+    signInOptions: string[];
+  };
 };
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 // @ts-ignore
 const AuthContext = createContext<AuthContextType>();
@@ -17,9 +29,17 @@ const AuthContext = createContext<AuthContextType>();
 function AuthProvider({ children }: { children: ReactNode }) {
   const [userInfo, setUserInfo] = useState<User | null>(null);
 
+  const firebaseUiConfig = {
+    signInFlow: "popup",
+    signInSuccessUrl: "/signedIn",
+    signInOptions: [firebase.auth.PhoneAuthProvider.PROVIDER_ID],
+  };
+
   const value = {
     user: userInfo,
     setUser: setUserInfo,
+    firebaseAuth: firebase.auth(),
+    firebaseUiConfig,
   };
 
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
