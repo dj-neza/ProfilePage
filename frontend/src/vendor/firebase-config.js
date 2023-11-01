c566f7a6c9f6998548e09ec12127685ffac589aa
dj-neza <neza.djukic@gmail.com>
2023-11-01 23:36:19 +0100

    Add firebase config and env file example

---
+++ frontend/src/vendor/firebase-config.js
@@ -0,0 +1,11 @@
+import firebase from 'firebase/compat/app';
+
+const apiKey = process.env['REACT_APP_FIREBASE_API']
+export const firebaseConfig = {
+  apiKey,
+  authDomain: "profile-38860.firebaseapp.com",
+  projectId: "profile-38860",
+  storageBucket: "profile-38860.appspot.com",
+  messagingSenderId: "606089239250",
+  appId: process.env['REACT_APP_FIREBASE_APP_ID'],
+};
