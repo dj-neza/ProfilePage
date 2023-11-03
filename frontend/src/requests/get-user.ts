6e26e33219418d1eed7cce8046c5dc16db573d33
dj-neza <neza.djukic@gmail.com>
2023-11-03 22:05:07 +0100

    Add getUser and updateUser functions

---
+++ frontend/src/requests/get-user.ts
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+const requestSuffix = process.env['REACT_APP_FIREBASE_FNS_SUFFIX']
+
+type GetUserParams = {
+  phoneNumber: string;
+}
+export function getUser({phoneNumber}: GetUserParams) {
+  return axios.get(`https://getuser${requestSuffix}`, {
+    params: {
+      phoneNumber
+    }
+  })
+}
---
+++ frontend/src/requests/update-user.ts
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const requestSuffix = process.env['REACT_APP_FIREBASE_FNS_SUFFIX']
+
+type UpdateUserParams = {
+  phoneNumber: string;
+  email?: string;
+  displayName?: string;
+}
+export function updateUser({phoneNumber, email, displayName}: UpdateUserParams) {
+  return axios.put(`https://updateuser${requestSuffix}`, {
+    phoneNumber,
+    email,
+    displayName
+  })
+}
