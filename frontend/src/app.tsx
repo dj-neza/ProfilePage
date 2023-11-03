f453693d89efe64ca1063783750d6bd833c1f571
dj-neza <neza.djukic@gmail.com>
2023-11-03 18:46:06 +0100

    Setup routes for app

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -1,10 +1,31 @@
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import { AuthProvider } from "./auth/auth-context";
 import { SignIn } from "./pages/sign-in";
+import { ProtectedRouteWrapper } from "./protected-route-wrapper";
+import { Profile } from "./pages/profile";
 
+function AppRoutes() {
+  return (
+    <Routes>
+      <Route
+        path="/profile"
+        element={
+          <ProtectedRouteWrapper>
+            <Profile />
+          </ProtectedRouteWrapper>
+        }
+      />
+      <Route path="/sign-in" element={<SignIn />} />
+      <Route path="/*" element={<Navigate to="/profile" />} />
+    </Routes>
+  );
+}
 export function App() {
   return (
     <AuthProvider>
-      <SignIn />
+      <BrowserRouter>
+        <AppRoutes />
+      </BrowserRouter>
     </AuthProvider>
   );
 }
