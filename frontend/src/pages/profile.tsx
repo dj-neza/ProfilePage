22246a7ad84fcd521db65399a97abbf2f889fe31
dj-neza <neza.djukic@gmail.com>
2023-11-03 19:03:30 +0100

    Add user info and logout button to profile page

--- frontend/src/pages/profile.tsx
+++ frontend/src/pages/profile.tsx
@@ -1,3 +1,19 @@
+import { useAuthContext } from "../auth/auth-context";
+
 export function Profile() {
-  return null;
+  const { user, logOut } = useAuthContext();
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "flex-start",
+      }}
+    >
+      <h3>{user?.displayName ?? "No name"}</h3>
+      <p>{user?.email ?? "No email"}</p>
+      <p>{user?.phoneNumber ?? "No phone"}</p>
+      <button onClick={logOut}>Log out</button>
+    </div>
+  );
 }
