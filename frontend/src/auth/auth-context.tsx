144e065d9a4cc338f0cea0491619e13a68b47bab
dj-neza <neza.djukic@gmail.com>
2023-11-03 18:48:41 +0100

    Store user data in local storage and update on authstate change

--- frontend/src/auth/auth-context.tsx
+++ frontend/src/auth/auth-context.tsx
@@ -1,23 +1,26 @@
-import { ReactNode, createContext, useContext, useState } from "react";
+import { ReactNode, createContext, useContext, useEffect } from "react";
 import { firebaseConfig } from "../vendor/firebase-config";
 import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
+import { useLocalStorage } from "../hooks/use-local-storage";
 
 type User = {
-  name: string;
-  email: string;
-  phone: string;
+  displayName: string | null;
+  email: string | null;
+  phoneNumber: string | null;
 };
 
 type AuthContextType = {
-  user: User | null;
+  user?: User | null;
   setUser: (user: User) => void;
+  isAuthenticated: boolean;
   firebaseAuth: firebase.auth.Auth;
   firebaseUiConfig: {
     signInFlow: string;
     signInSuccessUrl: string;
     signInOptions: string[];
   };
+  logOut: () => void;
 };
 
 // Initialize Firebase
@@ -27,19 +30,48 @@ firebase.initializeApp(firebaseConfig);
 const AuthContext = createContext<AuthContextType>();
 
 function AuthProvider({ children }: { children: ReactNode }) {
-  const [userInfo, setUserInfo] = useState<User | null>(null);
+  const [userInfo, setUserInfo] = useLocalStorage<User | null>(
+    "userInfo",
+    null,
+  );
+
+  const firebaseAuth = firebase.auth();
+
+  useEffect(() => {
+    const unregisterAuthObserver = firebase
+      .auth()
+      .onAuthStateChanged((user) => {
+        if (user) {
+          const { displayName, email, phoneNumber } = user;
+          setUserInfo({
+            displayName,
+            email,
+            phoneNumber,
+          });
+        } else {
+          setUserInfo(null);
+        }
+      });
+    return () => unregisterAuthObserver();
+  }, []);
+
+  const logOut = () => {
+    firebaseAuth.signOut();
+  };
 
   const firebaseUiConfig = {
     signInFlow: "popup",
-    signInSuccessUrl: "/signedIn",
+    signInSuccessUrl: "/profile",
     signInOptions: [firebase.auth.PhoneAuthProvider.PROVIDER_ID],
   };
 
   const value = {
     user: userInfo,
     setUser: setUserInfo,
-    firebaseAuth: firebase.auth(),
+    isAuthenticated: Boolean(userInfo),
+    firebaseAuth,
     firebaseUiConfig,
+    logOut,
   };
 
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
