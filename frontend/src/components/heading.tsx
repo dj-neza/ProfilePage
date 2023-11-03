9e693a67f4c6a3dcd48a0b5d08a71db2a092d17f
dj-neza <neza.djukic@gmail.com>
2023-11-04 00:12:46 +0100

    Add heading, paragraph and page wrapper components#

---
+++ frontend/src/components/heading.tsx
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const Heading = styled.h2({
+  fontWeight: 700,
+  fontSize: 24,
+  lineHeight: "28px",
+  color: "#050A30",
+});
---
+++ frontend/src/components/paragraph.tsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Paragraph = styled.p({
+  fontWeight: 400,
+  fontSize: 18,
+  lineHeight: "24px",
+  color: "#050A30",
+  whiteSpace: "pre-wrap",
+});
