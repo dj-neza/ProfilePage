1b53a030bbee1bd1fe92eb18e476bd9fa6b82444
dj-neza <neza.djukic@gmail.com>
2023-11-04 21:49:25 +0100

    Add option to vertically center content

--- frontend/src/components/content-wrapper.tsx
+++ frontend/src/components/content-wrapper.tsx
@@ -1,21 +1,26 @@
 import styled from "styled-components";
 
-export const ContentWrapper = styled.div({
-  boxSizing: "border-box",
-  width: "100%",
-  height: "90%",
-  marginTop: "20%",
-  backgroundColor: "white",
-  border: "1px solid #E5E5E5",
-  padding: 24,
-  display: "flex",
-  flexDirection: "column",
-  alignItems: "center",
-  "@media(min-width: 480px)": {
-    width: 360,
-    minHeight: "50%",
-    height: "unset",
-    borderRadius: 16,
-    marginTop: "unset",
-  },
-});
+export const ContentWrapper = styled.div<{ $verticallyCenter?: boolean }>(
+  ({ $verticallyCenter = false }) => ({
+    boxSizing: "border-box",
+    width: "100%",
+    height: "90%",
+    marginTop: "20%",
+    backgroundColor: "white",
+    border: "1px solid #E5E5E5",
+    padding: 24,
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    ...($verticallyCenter && {
+      justifyContent: "center",
+    }),
+    "@media(min-width: 480px)": {
+      width: 360,
+      minHeight: "50%",
+      height: "unset",
+      borderRadius: 16,
+      marginTop: "unset",
+    },
+  }),
+);
