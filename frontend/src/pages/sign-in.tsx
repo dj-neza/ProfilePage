984bfaa07b6d417e3ac87757a3ad8f675fba683f
dj-neza <neza.djukic@gmail.com>
2023-11-04 21:47:03 +0100

    Use new loader in signin

--- frontend/src/pages/sign-in.tsx
+++ frontend/src/pages/sign-in.tsx
@@ -2,29 +2,10 @@ import { Navigate } from "react-router-dom";
 import { useAuthContext } from "../auth/auth-context";
 // import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
 import FirebaseAuth from "../components/firebase-auth";
-import styled from "styled-components";
 import { PageWrapper } from "../components/page-wrapper";
 import { Heading } from "../components/heading";
+import { ContentWrapper } from "../components/content-wrapper";
 
-const ContentWrapper = styled.div({
-  boxSizing: "border-box",
-  width: "100%",
-  height: "90%",
-  marginTop: "20%",
-  backgroundColor: "white",
-  border: "1px solid #E5E5E5",
-  padding: 24,
-  display: "flex",
-  flexDirection: "column",
-  alignItems: "center",
-  "@media(min-width: 480px)": {
-    width: 360,
-    minHeight: "50%",
-    height: "unset",
-    borderRadius: 16,
-    marginTop: "unset",
-  },
-});
 export function SignIn() {
   const { firebaseAuth, firebaseUiConfig } = useAuthContext();
   const { isAuthenticated } = useAuthContext();
