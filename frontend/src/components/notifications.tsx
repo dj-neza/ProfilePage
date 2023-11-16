6752896701e4ca3966918b1175e9b5c5caebba3e
dj-neza <neza.djukic@gmail.com>
2023-11-17 00:32:01 +0100

    Remove console.log

--- frontend/src/components/notifications.tsx
+++ frontend/src/components/notifications.tsx
@@ -56,7 +56,6 @@ export function Notification({ message, onClose }: NotificationConfig) {
 
 export function Notifications() {
   const { notifications } = useNotificationContext()
-  console.log(notifications)
   return (
     <NotificationsListContainer>
       {notifications.map((notification) => (
